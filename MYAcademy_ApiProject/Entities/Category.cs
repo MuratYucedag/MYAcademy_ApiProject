26ea25aaa2980919a92243f59ec26fc3b7d5887c
MuratYucedag <yucedagmurat23@gmail.com>
2026-09-22 20:32:55 +0300

    Add project files.

---
+++ MYAcademy_ApiProject/Entities/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace MYAcademy_ApiProject.Entities
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
---
+++ MYAcademy_ApiProject/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace MYAcademy_ApiProject.Entities
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int ProductStock { get; set; }
+        public decimal ProductPrice { get; set; }
+        public bool IsCritic { get; set; }
+    }
+}
